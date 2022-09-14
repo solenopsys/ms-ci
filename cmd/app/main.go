03a44b2f3643ed6586004df6be26d50ed068abc7
alexstorm <solenopsys:)>
2022-09-15 00:08:31 +0300

    fixes

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -85,7 +85,7 @@ func main() {
 	name := "solenopsys-hsm-ci"
 	kube.CreateJobFunc(clientSet,
 		name,
-		"ci-build-job25",
+		"ci-build-job26",
 		"git.alexstorm.solenopsys.org",
 		"linux/amd64,linux/arm64",
 		"/workspace/"+name+"/cic/jobs/test",
